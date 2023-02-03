68bed9f42c54ea05018c35c13e2abbda63b88e56
Hvbert1 <henryjiafu@gmail.com>
2023-02-03 13:51:02 +1100

    refactor winCheck

--- script.js
+++ script.js
@@ -51,38 +51,23 @@ const controller = (() => {
     function turn(cellId, player) {
         board[cellId] = player;
         document.getElementById(cellId).innerText = player;
+        winCheck(board, player)
     }
 
-    function winCheck() {
-        if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X' ||
-            board[3] == 'X' && board[4] == 'X' && board[5] == 'X' ||
-            board[6] == 'X' && board[7] == 'X' && board[8] == 'X' ||
-            board[0] == 'X' && board[3] == 'X' && board[6] == 'X' ||
-            board[1] == 'X' && board[4] == 'X' && board[7] == 'X' ||
-            board[2] == 'X' && board[5] == 'X' && board[8] == 'X' ||
-            board[0] == 'X' && board[4] == 'X' && board[8] == 'X' ||
-            board[2] == 'X' && board[4] == 'X' && board[6] == 'X'
+    function winCheck(board, player) {
+        if (board[0] == player && board[1] == player && board[2] == player ||
+            board[3] == player && board[4] == player && board[5] == player ||
+            board[6] == player && board[7] == player && board[8] == player ||
+            board[0] == player && board[3] == player && board[6] == player ||
+            board[1] == player && board[4] == player && board[7] == player ||
+            board[2] == player && board[5] == player && board[8] == player ||
+            board[0] == player && board[4] == player && board[8] == player ||
+            board[2] == player && board[4] == player && board[6] == player
         ) {
             alert('You win!');
             gameBoard.reset();
             gameBoard.createBoard();
         }
-
-        else if (
-            board[0] == 'O' && board[1] == 'O' && board[2] == 'O' ||
-            board[3] == 'O' && board[4] == 'O' && board[5] == 'O' ||
-            board[6] == 'O' && board[7] == 'O' && board[8] == 'O' ||
-            board[0] == 'O' && board[3] == 'O' && board[6] == 'O' ||
-            board[1] == 'O' && board[4] == 'O' && board[7] == 'O' ||
-            board[2] == 'O' && board[5] == 'O' && board[8] == 'O' ||
-            board[0] == 'O' && board[4] == 'O' && board[8] == 'O' ||
-            board[2] == 'O' && board[4] == 'O' && board[6] == 'O'
-        ) {
-            alert('You lose!');
-            gameBoard.reset();
-            gameBoard.createBoard();
-        }
-
         else if(count == 6) {
             alert('Draw');
             gameBoard.reset();
